9fe6f9188f56f970f3563dd8e9f6957b3e6273d3
Sahar Nafisi <sahar.nafisi95@gmail.com>
2017-11-06 10:16:57 +0330

    reading from file bug fixed

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import csv
 
-with open('Youtube01-Psy.csv') as csvfile:
+with open('./dataset/Youtube01-Psy.csv' , encoding="utf8") as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
         print(row)
         print(row[0])
         print(row[0], row[1], row[2])
+
+
