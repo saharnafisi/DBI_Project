ab13c81ecda67acef0cc061b0b81186da8deae03
Alireza Mohammadi <alireza_6313@Yahoo.com>
2017-11-17 16:29:36 +0330

    functions for removing unknown '\ufeff' character and creating set of words implemented

--- main.py
+++ main.py
@@ -33,18 +33,26 @@ def standardize_urls(comments):
     return new_review
 
 
+def remove_special_chars(comments):
+    new_review = []
+    for comment in comments:
+        new_review.append(comment.replace(u'\ufeff', ''))
+
+    return new_review
+
+
 def tokenize_comments(comments):
     # convert each comment content to tokens
     tokenized_comments = [word_tokenize(comment) for comment in comments]
     return tokenized_comments
 
 
-def delete_punctuation(tokens):
+def delete_punctuation(tokenized_comments):
     # delete punctuation marks from tokens
     regex = re.compile('[%s]' % re.escape(string.punctuation))
     tokenized_comment_no_punctuation = []
 
-    for review in tokens:
+    for review in tokenized_comments:
         new_review = []
         for token in review:
             new_token = regex.sub(u'', token)
@@ -55,10 +63,10 @@ def delete_punctuation(tokens):
     return tokenized_comment_no_punctuation
 
 
-def delete_stopwords(tokens):
+def delete_stopwords(tokenized_comments):
 
     tokenized_comments_no_stopwords = []
-    for comment in tokens:
+    for comment in tokenized_comments:
         new_term_vector = []
         for word in comment:
             if not word in stopwords.words('english'):
@@ -68,10 +76,10 @@ def delete_stopwords(tokens):
     return tokenized_comments_no_stopwords
 
 
-def stemm_tokens(tokens):
+def stemm_tokens(tokenized_comments):
     stemmed_comments = []
     ls = LancasterStemmer()
-    for comment in tokens:
+    for comment in tokenized_comments:
         new_term_vector = []
         for word in comment:
             new_term_vector.append(ls.stem(word))
@@ -80,11 +88,22 @@ def stemm_tokens(tokens):
     return stemmed_comments
 
 
+def create_set_of_words(tokenized_comments):
+    set_of_words = set()
+    for comment in tokenized_comments:
+        for token in comment:
+            set_of_words.add(token)
+
+    return set_of_words
+
+
 if __name__ == "__main__":
     comments = readFromFile()
     comments = standardize_urls(comments)
+    comments = remove_special_chars(comments)
     tokenized_comments = tokenize_comments(comments)
     tokenized_comments = delete_punctuation(tokenized_comments)
     tokenized_comments = delete_stopwords(tokenized_comments)
     tokenized_comments = stemm_tokens(tokenized_comments)
-    print(tokenized_comments)
+    set_of_words = create_set_of_words(tokenized_comments)
+    print(set_of_words)
